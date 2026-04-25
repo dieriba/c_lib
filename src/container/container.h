ba7482f743f55f217af00a2a63d4fc548d7390c7
dieriba <dieriba.pro@gmail.com>
2026-04-25 20:25:35 +0200

    update header cont

--- src/container/container.h
+++ src/container/container.h
@@ -1,46 +1,45 @@
-#ifndef __CONTAINER__H
-#define __CONTAINER__H
+#ifndef CONTAINER_H
+#define CONTAINER_H
 
-#include <stdbool.h>
-#include "d_types.h"
 #include "d_bits.h"
+#include "d_types.h"
 
 #define DEFAULT_CAPACITY 0x10
-
-typedef struct _Container
-{
-    void *data;
-    usize len;
-    usize capacity;
-    usize elem_size;
-    DBits8 opts;
-} Container;
 
 typedef enum e_container_opts
 {
     CNT_OPT_NONE = 0,
-    CNT_OPT_ZERO_SENTINEL = 1u << 0,
+    CNT_OPT_ZERO_SENTINEL = 1 << 0,
 } ContainerOpts;
 
-Container *container_new(usize elem_size, usize capacity, DBits8 opts);
-Container *container_new_from(const Container *src);
-void container_destroy(Container *container);
+typedef struct _Container
+{
+    void    *data;
+    usize   len;
+    usize   capacity;
+    usize   elem_size;
+    DBits8  opts;
+} Container;
 
-Result container_increase_capacity_if_needed(Container *container, usize nb_elem_to_copy);
-Result container_insert(Container *dst, usize dst_pos, const void *data, usize len);
-Result container_append(Container *dst, const Container *src);
-Result container_prepend(Container *dst, const Container *src);
+Result      container_init(Container *container, usize elem_size, usize capacity, DBits8 opts);
+Container   *container_new(usize elem_size, usize capacity, DBits8 opts);
+Container   *container_new_from(const Container *src);
+void        container_destroy(Container *container);
 
-void *container_get_data(Container *container);
-const void *container_get_cdata(const Container *container);
-usize container_get_len(const Container *container);
-usize container_get_capacity(const Container *container);
-usize container_get_elem_size(const Container *container);
-DBits8 container_get_opts(const Container *container);
-bool container_has_opt(const Container *container, ContainerOpts opt);
+Result      container_increase_capacity_if_needed(Container *container, usize nb_elem_to_copy);
 
-Result container_set_opt(Container *container, ContainerOpts opt);
-void container_unset_opt(Container *container, ContainerOpts opt);
-Result container_set_opts(Container *container, DBits8 opts);
+void        *container_get_data(Container *container);
+usize       container_get_len(const Container *container);
+usize       container_get_capacity(const Container *container);
+usize       container_get_elem_size(const Container *container);
+DBits8      container_get_opts(const Container *container);
+
+Result      container_insert(Container *dst, usize dst_pos, const void *data, usize len);
+Result      container_append(Container *dst, const Container *src);
+Result      container_prepend(Container *dst, const Container *src);
+Result      container_push(Container *container, const void *elem);
+Result      container_pop(Container *container, void *out_elem);
+Result      container_swap_remove(Container *container, usize index, void *out_elem);
+void        container_clear(Container *container);
 
 #endif
