497c019d194588e628077a4561f4652f329576b1
dieriba <dieriba.pro@gmail.com>
2026-05-07 13:18:10 +0200

    better init module

--- init_module.sh
+++ init_module.sh
@@ -1,36 +1,46 @@
 #!/bin/sh
 
+set -eu
+
 module_name=$1
+
+
+
 test_directory=tests
 src_directory=src
 includes_directory=includes
 header_filename=${1}.h
 header_guard=$(printf $header_filename | tr '[:lower:]' '[:upper:]' | tr '.' '_')
 
-mkdir -p $src_directory/$module_name
-
-cat <<EOF >$src_directory/$module_name/${module_name}.c
-#include "$header_filename"
-
+create_file() {
+    local file_name=$1
+    local file_content=$2
+    local file_parent_dir="$(dirname "$file_name")"
+    mkdir -p $file_parent_dir
+cat << EOF >$file_name
+$file_content
 EOF
+}
 
-cat <<EOF >$includes_directory/$header_filename
+create_file $src_directory/$module_name/${module_name}.c  "#include \"$header_filename\""
+create_file $includes_directory/$header_filename  "$(cat <<EOF
 #ifndef $header_guard
 #define $header_guard
-
 #include "d_types.h"
-
-
+   
 #endif
 EOF
+)"
 
-mkdir -p $test_directory/$module_name
-
-cat <<EOF >$test_directory/$module_name/test.c
+create_file $test_directory/$module_name/test.c "$(cat <<EOF 
 #include "d_test.h"
-
-int main()
+int main() 
 {
-
+    
 }
 EOF
+)"
+
+
+
+
